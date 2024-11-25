0b25c3441063ae9f92507c6a1c6a9bdb441b67e8
ChristopheCai <caichristo@cy-tech.fr>
2024-11-25 14:40:21 +0100

    Fix SpriteGenerator

--- UnityProject/Assets/Scripts/Tools/SpriteGenerator.cs
+++ UnityProject/Assets/Scripts/Tools/SpriteGenerator.cs
@@ -4,9 +4,10 @@ using UnityEditor;
 
 public class SpriteGenerator : MonoBehaviour
 {
-    public Camera captureCamera;
-    public Transform models;
-    public bool overwrite;
+    [SerializeField] private Camera captureCamera;
+    [SerializeField] private Transform models;
+    [SerializeField] private bool overwrite;
+    [SerializeField] private bool generate;
     private string outputFolder = "Assets/Resources/Items/Sprites2D/";
 
     private void Start()
@@ -19,48 +20,50 @@ public class SpriteGenerator : MonoBehaviour
 
     private void Update()
     {
-        RenderSprite();
+        if (Application.isPlaying && generate)
+        {
+            GenerateSprites();
+            generate = false;
+        }
     }
 
-    void RenderSprite()
+    private void GenerateSprites()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        for (int i = 0; i < models.childCount; i++)
         {
-            for (int i = 0; i < models.childCount; i++)
+            GameObject model = models.GetChild(i).gameObject;
+
+            model.SetActive(true);
+
+            RenderTexture rt = new RenderTexture(512, 512, 24);
+            rt.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8A8_UNorm;
+            captureCamera.targetTexture = rt;
+
+            Texture2D screenshot = new Texture2D(512, 512, TextureFormat.RGBA32, false);
+
+            captureCamera.Render();
+            RenderTexture.active = rt;
+            screenshot.ReadPixels(new Rect(0, 0, 512, 512), 0, 0);
+            screenshot.Apply(); // Applique les changements à la texture
+
+            RenderTexture.active = null;
+
+            string filePath = outputFolder + model.name + ".png";
+            if (!overwrite && File.Exists(filePath))
             {
-                GameObject model = models.GetChild(i).gameObject;
-
-                model.SetActive(true);
-
-                RenderTexture rt = new RenderTexture(512, 512, 24);
-                rt.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8A8_UNorm;
-                captureCamera.targetTexture = rt;
-
-                Texture2D screenshot = new Texture2D(512, 512, TextureFormat.RGBA32, false);
-
-                captureCamera.Render();
-                RenderTexture.active = rt;
-                screenshot.ReadPixels(new Rect(0, 0, 512, 512), 0, 0);
-                screenshot.Apply(); // Applique les changements à la texture
-
-                RenderTexture.active = null;
-
-                string filePath = outputFolder + model.name + ".png";
-                if (!overwrite && File.Exists(filePath))
-                {
-                    Debug.Log("File already exists, skipping save: " + filePath);
-                }
-                else
-                {
-                    File.WriteAllBytes(filePath, screenshot.EncodeToPNG());
-                    Debug.Log(model.name + " has been saved as a PNG into: " + outputFolder);
-                }
-
-                captureCamera.targetTexture = null;
-                Destroy(rt);
-                model.SetActive(false);
+                Debug.Log("File already exists, skipping save: " + filePath);
             }
-            AssetDatabase.Refresh();
+            else
+            {
+                File.WriteAllBytes(filePath, screenshot.EncodeToPNG());
+                Debug.Log(model.name + " has been saved as a PNG into: " + outputFolder);
+            }
+
+            captureCamera.targetTexture = null;
+            Destroy(rt);
+            model.SetActive(false);
         }
+        AssetDatabase.Refresh();
+        Debug.Log("All models have been saved.");
     }
 }
